c4e10ad113528abf056a261b6e6027d01d9f8135
Kanishka Misra <menogetusername@gmail.com>
2020-12-14 22:00:08 -0600

    adaptation

--- minicons/scorer.py
+++ minicons/scorer.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Union, List, Dict, Optional, Callable
+from typing import Iterable, Union, List, Dict, Optional, Callable, Tuple
 
 import torch
 from transformers import AutoModelForCausalLM, AutoModelForMaskedLM, AutoTokenizer
@@ -23,6 +23,9 @@ class LMScorer:
     def prepare_text(self, text: Union[str, List[str]]) -> Union[str, List[str]]:
         raise NotImplementedError
 
+    def prime_text(self, preamble: Union[str, List[str]], stimuli: Union[str, List[str]]) -> Tuple:
+        raise NotImplementedError
+
     def seq_score(self, batch: Iterable):
         raise NotImplementedError
     
@@ -33,10 +36,16 @@ class LMScorer:
         reduced = list(map(lambda x: reduce(x, *args).tolist(), logprob))
         
         return reduced
-
-
-    def encode(self, text: Union[str, List[str]], manual_special: bool = True, return_tensors: Optional[str] = 'pt') -> Dict:
+    
+    def adapt_score(self, preamble: Iterable, stimuli: Iterable, reduce: Callable = torch.mean, *args) -> Union[float, List[float]]:
         
+        result = self.logprobs(self.prime_text(preamble, stimuli))
+        logprob, _ = list(zip(*result))
+        reduced = list(map(lambda x: reduce(x, *args).tolist(), logprob))
+        
+        return reduced
+
+    def encode(self, text: Union[str, List[str]], manual_special: bool = True, return_tensors: Optional[str] = 'pt') -> Dict:       
         sentences = [text] if isinstance(text, str) else text
 
         if manual_special:
@@ -105,14 +114,64 @@ class MaskedLMScorer(LMScorer):
                 
                 attention_masked_list.append(attention_masked)
                 token_ids_masked_list.append(token_ids_masked)
-            masked_tensors.append((torch.stack(token_ids_masked_list), torch.stack(attention_masked_list), effective_token_ids, len(mask_indices)))
+            masked_tensors.append((torch.stack(token_ids_masked_list), torch.stack(attention_masked_list), effective_token_ids, len(mask_indices), 0))
         
+        return masked_tensors
+
+    def prime_text(self, preamble: Union[str, List[str]] , stimuli: Union[str, List[str]]) -> Tuple:
+   
+        preamble_text = [preamble] if isinstance(preamble, str) else preamble
+        preamble_encoded = self.encode(preamble_text, False)['input_ids']
+        preamble_lens = []
+        for preamble_tokens in preamble_encoded:
+            preamble_lens.append(len([token for token in preamble_tokens if token != self.pad_token_id and token != self.sep_token_id]))
+        
+        sentences = [preamble + " " + stimuli] if isinstance(preamble, str) else [p + " " + s for p, s in list(zip(preamble, stimuli))]
+            
+        # idea is to tokenize and then create batches of tokenized instances,
+        # but with each token in the sequence replaced by the mask token. 
+
+        encoded = self.encode(sentences, manual_special = False)
+
+        token_idx = encoded['input_ids']
+        attention_masks = encoded['attention_mask']
+
+        masked_tensors = [] # token ids, attention masks, lengths
+
+        for i, (token_ids, attention_mask) in enumerate(zip(token_idx, attention_masks)):
+            token_ids = torch.tensor(token_ids)
+            # final_lengths = len(token_ids) - 2
+            attention_mask = torch.tensor(attention_mask)
+
+            token_ids_masked_list = []
+            attention_masked_list = []
+            
+            effective_token_ids = [token for j, token in enumerate(token_ids) if token != self.pad_token_id and token != self.cls_token_id and token != self.sep_token_id and j >= preamble_lens[i]]
+            effective_length = len(effective_token_ids) + preamble_lens[i]
+
+
+            mask_indices = []
+            mask_indices = [[mask_pos] for mask_pos in range(preamble_lens[i], effective_length+1)]
+
+            # We don't mask the [CLS], [SEP] for now for PLL
+            mask_indices = mask_indices[:-1]
+
+            mask_token_id = self.mask_token_id
+            for mask_set in mask_indices:
+                token_ids_masked = token_ids.clone()
+                token_ids_masked[mask_set] = mask_token_id
+                attention_masked = attention_mask.clone()
+
+                attention_masked_list.append(attention_masked)
+                token_ids_masked_list.append(token_ids_masked)
+            masked_tensors.append((torch.stack(token_ids_masked_list), torch.stack(attention_masked_list), effective_token_ids, len(mask_indices), preamble_lens[i]))
+
         return masked_tensors
 
     def logprobs(self, batch: Iterable) -> Union[float, List[float]]:
         
         # takes in prepared text and returns scores for each sentence in batch
-        token_ids, attention_masks, effective_token_ids, lengths = list(zip(*batch))
+        token_ids, attention_masks, effective_token_ids, lengths, offsets = list(zip(*batch))
         token_ids = torch.cat(token_ids)
         attention_masks = torch.cat(attention_masks)
         token_ids = token_ids.to(self.device)
@@ -121,7 +180,7 @@ class MaskedLMScorer(LMScorer):
 
         sent_tokens = list(map(lambda x: self.tokenizer.convert_ids_to_tokens(x.tolist()), effective_token_ids.split(lengths)))
         
-        indices = list(itertools.chain.from_iterable([list(range(1,n+1)) for n in lengths]))
+        indices = list(itertools.chain.from_iterable([list(range(o,o+n)) for n, o in zip(lengths, offsets)]))
         with torch.no_grad():
             output = self.model(token_ids, attention_mask = attention_masks)
             logits = output.logits[torch.arange(sum(lengths)), indices]
@@ -158,11 +217,25 @@ class IncrementalLMScorer(LMScorer):
         self.model.to(self.device)
         self.model.eval()
     
-    def prepare_text(self, text: Union[str, List[str]]) -> Union[str, List[str]]:
-        return self.encode(text)
+    def prepare_text(self, text: Union[str, List[str]]) -> Tuple:
+        encoded = self.encode(text)
+        offsets = [0] * len(encoded)
+        return encoded, offsets
+    
+    def prime_text(self, preamble: Union[str, List[str]], stimuli: Union[str, List[str]]) -> Tuple:
+        preamble_text = [preamble] if isinstance(preamble, str) else preamble
+        preamble_encoded = self.encode(preamble_text, False)['input_ids']
+        preamble_lens = []
+        for preamble_tokens in preamble_encoded:
+            preamble_lens.append(len([token for token in preamble_tokens if token != self.tokenizer.pad_token_id and token != self.tokenizer.sep_token_id]))
+        
+        sentences = [preamble + " " + stimuli] if isinstance(preamble, str) else [p + " " + s for p , s in list(zip(preamble, stimuli))]
+            
+        return self.encode(sentences), preamble_lens
 
     def logprobs(self, batch: Iterable) -> Union[float, List[float]]:
         
+        batch, offsets = batch
         ids = batch["input_ids"]
         ids = ids.to(self.device)
         attention_masks = batch["attention_mask"]
@@ -182,7 +255,7 @@ class IncrementalLMScorer(LMScorer):
             sent_tokens = [
                 tok
                 for i, tok in enumerate(batch.tokens(sent_index))
-                if sent_nopad_mask[i] and i != 0
+                if sent_nopad_mask[i] and i > offsets[sent_index]
             ]
 
             # sent_ids.shape = [len(text[sent_index]) + 1]
@@ -198,6 +271,7 @@ class IncrementalLMScorer(LMScorer):
             sent_log_probs = sent_ids_scores - sent_logits.logsumexp(1)
 
             sent_log_probs = sent_log_probs.type(torch.DoubleTensor)
+            sent_log_probs = sent_log_probs[offsets[sent_index]:]
             # sent_ids = sent_ids.type(torch.LongTensor)
             outputs.append((sent_log_probs, sent_tokens))
             # output = (sent_log_probs.sum(), sent_ids, sent_tokens)
